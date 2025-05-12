ec839bed6c1033e5629690f4f5047929820451ba
Nunes <clement.nunes@efrei.net>
2025-05-12 11:27:26 +0200

    Add BankDetailController.cs and InvoiceService.cs for Invoice support

---
+++ InvoicingSys.CoreApi/Core/Controllers/BankDetailController.cs
@@ -0,0 +1,111 @@
+﻿using System.Net;
+using InvoicingSys.CoreApi.Core.Blueprints;
+using InvoicingSys.CoreApi.Core.Entities;
+using Microsoft.AspNetCore.Mvc;
+using InvoicingSys.CoreApi.Core.Services;
+using Swashbuckle.AspNetCore.Annotations;
+
+namespace InvoicingSys.CoreApi.Core.Controllers;
+
+[Route("/core/bankDetails")]
+[ApiController]
+public class BankDetailController : ControllerBase
+{
+    private readonly BankDetailService _bankDetailService;
+
+    public BankDetailController(BankDetailService bankDetailService)
+    {
+        _bankDetailService = bankDetailService;
+    }
+
+    [HttpPost("add")]
+    [SwaggerOperation(Summary = "Creates an bankDetail")]
+    [Produces("application/json")]
+    [ProducesResponseType(typeof(BankDetail), 200)]
+    [ProducesResponseType(typeof(ProblemDetails), (int)HttpStatusCode.NotFound)]
+    public ActionResult<BankDetail> Post([FromBody] BankDetailBlueprint body)
+    {
+        BankDetail? bankDetail = null;
+
+        try
+        {
+            bankDetail = _bankDetailService.CreateBankDetail(
+                body.Location, 
+                body.OwnerName, 
+                body.Iban, 
+                body.Bic);
+        }
+        catch (Exception e)
+        {
+            if (e is ArgumentNullException)
+                return BadRequest();
+
+            Console.WriteLine(e);
+            throw;
+        }
+
+        return bankDetail;
+    }
+
+    [HttpPost("")]
+    [SwaggerOperation(Summary = "Creates bankDetails")]
+    [Produces("application/json")]
+    [ProducesResponseType(typeof(IEnumerable<BankDetail>), 200)]
+    [ProducesResponseType(typeof(ProblemDetails), (int)HttpStatusCode.NotFound)]
+    public ActionResult<IEnumerable<BankDetail>> Post([FromBody] IEnumerable<BankDetailBlueprint> body)
+    {
+        BankDetail? bankDetail = null;
+        var bankDetails = new List<BankDetail>();
+        
+        foreach (var bankDetailBlueprint in body)
+            try
+            {
+                bankDetail = _bankDetailService.CreateBankDetail(
+                    bankDetailBlueprint.Location, 
+                    bankDetailBlueprint.OwnerName, 
+                    bankDetailBlueprint.Iban, 
+                    bankDetailBlueprint.Bic);
+
+                bankDetails.Add(bankDetail);
+            }
+            catch (Exception e)
+            {
+                if (e is ArgumentNullException)
+                    return BadRequest();
+
+                Console.WriteLine(e);
+                throw;
+            }
+        
+        return bankDetails;
+    }
+
+    [HttpGet("")]
+    [SwaggerOperation(Summary = "Returns all bankDetails")]
+    [Produces("application/json")]
+    [ProducesResponseType(typeof(IEnumerable<BankDetail>), StatusCodes.Status200OK)]
+    [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status404NotFound)]
+    public ActionResult<IEnumerable<BankDetail>> GetBankDetails()
+    {
+        var bankDetails = _bankDetailService.GetBankDetails();
+
+        return bankDetails;
+    }
+
+
+    [HttpGet("{bankDetailId:guid}")]
+    [SwaggerOperation(Summary = "Return BankDetail from BankDetailId")]
+    [Produces("application/json")]
+    [ProducesResponseType(typeof(BankDetail), StatusCodes.Status200OK)]
+    [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status404NotFound)]
+    public ActionResult<BankDetail> GetBankDetail([FromQuery] Guid bankDetailId)
+    {
+        var bankDetail = _bankDetailService.GetBankDetailById(bankDetailId);
+
+        if (bankDetail is null)
+            return NotFound();
+
+        return bankDetail;
+    }
+    
+}
---
+++ InvoicingSys.CoreApi/Core/Services/InvoiceService.cs
@@ -0,0 +1,70 @@
+﻿using InvoicingSys.CoreApi.Core.DataContext.DBContexts;
+using InvoicingSys.CoreApi.Core.Entities;
+
+namespace InvoicingSys.CoreApi.Core.Services;
+
+public class InvoicingService
+{
+    private readonly ApplicationDbContext _context;
+    
+    public InvoicingService(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+    
+    public Order? _order = default!;
+    public DateTime? _dueDate = default!;
+    public DateTime? _invoicingDate = default!;
+    public BankDetail? _bankDetail = default!;
+    
+    public Invoice CreateInvoice(Order? order, DateTime? dueDate, BankDetail? bankDetail)
+    {
+        if (order is null)
+            throw new ArgumentNullException(nameof(order), "Order cannot be null");
+        
+        if (dueDate is null)
+            throw new ArgumentNullException(nameof(dueDate), "DueDate name cannot be null");
+        
+        if (bankDetail is null)
+            throw new ArgumentNullException(nameof(bankDetail), "BankDetail cannot be null");
+
+        Invoice invoice = new Invoice(order, dueDate, DateTime.Today, bankDetail);
+        
+        _context.Invoices.Add(invoice);
+        _context.SaveChanges();
+        return invoice;
+    }
+    
+    public Invoice ModifyInvoice(Invoice invoice, Order? order, DateTime? dueDate, BankDetail? bankDetail)
+    {
+        if(order is not null)
+            invoice.Order = order;
+            
+        if(dueDate is not null)
+            invoice.DueDate = dueDate;
+        
+        if(bankDetail is not null)
+            invoice.BankDetail = bankDetail;
+        
+        _context.SaveChanges();
+        
+        return invoice;
+    }
+
+    public bool DeleteInvoice(Invoice invoice)
+    {
+        _context.Invoices.Remove(invoice);
+        _context.SaveChanges();
+        return true;
+    }
+
+    public List<Invoice> GetInvoices()
+    {
+        return _context.Invoices.ToList();
+    }
+
+    public Invoice? GetInvoiceById(Guid invoiceId)
+    {
+        return _context.Invoices.FirstOrDefault(i => i.Id == invoiceId);
+    }
+}
